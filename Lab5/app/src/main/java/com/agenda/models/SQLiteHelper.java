48a00b61968302370e071bbca9c8c9efc1470a5a
Joel May <joel@jmay.us>
2017-09-29 13:34:23 -0500

    SQL

--- Lab5/app/src/main/java/com/agenda/models/SQLiteHelper.java
+++ Lab5/app/src/main/java/com/agenda/models/SQLiteHelper.java
@@ -10,57 +10,64 @@ import android.util.Log;
  * should be called directly.
  */
 public class SQLiteHelper extends SQLiteOpenHelper {
-	
-	// Table name
-	public static final String TABLE_EVENTS = "events";
-	
-	// Table columns
-	public static final String COLUMN_ID = "_id";
-	public static final String COLUMN_TITLE = "title";
-	public static final String COLUMN_LOCATION = "location";
-	public static final String COLUMN_START = "start";
-	public static final String COLUMN_END = "end";
-	public static final String COLUMN_DETAILS = "details";
-	
-	// Database name
-	private static final String DATABASE_NAME = "agenda.db";
-	
-	// Increment this number to clear everything in database
-	private static final int DATABASE_VERSION = 1;
+    
+    // Table name
+    public static final String TABLE_EVENTS = "events";
+    
+    // Table columns
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TITLE = "title";
+    public static final String COLUMN_LOCATION = "location";
+    public static final String COLUMN_START = "start";
+    public static final String COLUMN_END = "end";
+    public static final String COLUMN_DETAILS = "details";
+    
+    // Database name
+    private static final String DATABASE_NAME = "agenda.db";
+    
+    // Increment this number to clear everything in database
+    private static final int DATABASE_VERSION = 1;
 
-	/**
-	 * Returns an instance of this helper object given the activity
-	 * @param context
-	 */
-	public SQLiteHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
-	}
+    /**
+     * Returns an instance of this helper object given the activity
+     * @param context
+     */
+    public SQLiteHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
-	 */
-	@Override
-	public void onCreate(SQLiteDatabase db) {
-		/*
-		 * TODO: Create database table "events"
-		 * COLUMN_ID should be of type "integer primary key autoincrement"
-		 * All other columns should be of type "text not null"
-		 * Columns names have been created as constants at top of this class
-		 */
-	}
+    /*
+     * (non-Javadoc)
+     * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
+     */
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        /*
+         * TODO: Create database table "events"
+         * COLUMN_ID should be of type "integer primary key autoincrement"
+         * All other columns should be of type "text not null"
+         * Columns names have been created as constants at top of this class
+         */
+        db.execSQL("CREATE TABLE " + TABLE_EVENTS + " (" +
+                COLUMN_ID + " integer primary key autoincrement, " +
+                COLUMN_TITLE + " text not null, " +
+                COLUMN_LOCATION + " text not null, " +
+                COLUMN_START + " text not null, " +
+                COLUMN_END + " text net null, " +
+                COLUMN_DETAILS + " text not null)");
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
-	 */
-	@Override
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		Log.w(SQLiteHelper.class.getName(),
-				"Upgrading database from version " + oldVersion + " to "
-						+ newVersion + ", which will destroy all old data");
-		db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
-		onCreate(db);
-	}
+    /*
+     * (non-Javadoc)
+     * @see android.database.sqlite.SQLiteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, int, int)
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.w(SQLiteHelper.class.getName(),
+                "Upgrading database from version " + oldVersion + " to "
+                        + newVersion + ", which will destroy all old data");
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENTS);
+        onCreate(db);
+    }
 
 }
