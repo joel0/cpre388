0cd1a496e8880817e916d50ce856d199a84ec3ba
Joel May <joel@jmay.us>
2017-11-01 22:36:49 -0500

    Thread pool and UI

---
+++ Homework4/app/src/main/java/cpre388/jmay/homework4/BusyRunnable.java
@@ -0,0 +1,29 @@
+package cpre388.jmay.homework4;
+
+import android.os.Process;
+
+/**
+ * Created by jmay on 2017-11-01.
+ */
+
+class BusyRunnable implements Runnable {
+
+    public Thread mThread = null;
+    private RunActivity mParent;
+
+    public BusyRunnable(RunActivity parent) {
+        mParent = parent;
+    }
+
+    @Override
+    public void run() {
+        android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND);
+        mThread = Thread.currentThread();
+
+        //noinspection StatementWithEmptyBody
+        for (int i = 0; i < 1000000000; i++) {
+            // nothing
+        }
+        mParent.taskEnded();
+    }
+}
--- Homework4/app/src/main/java/cpre388/jmay/homework4/RunActivity.java
+++ Homework4/app/src/main/java/cpre388/jmay/homework4/RunActivity.java
@@ -1,19 +1,25 @@
 package cpre388.jmay.homework4;
 
 import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class RunActivity extends AppCompatActivity {
+    private static final int TASK_COMPLETE = 1;
     private int mInitPoolSize;
     private int mMaxPoolSize;
     private int mKeepAlive;
@@ -22,6 +28,8 @@ public class RunActivity extends AppCompatActivity {
 
     private int mCompletedTasks = 0;
     private BlockingQueue<Runnable> mRunnableQueue = new LinkedBlockingQueue<>();
+    private ThreadPoolExecutor mThreadPoolExecutor;
+    private Handler mHandler;
 
     private TextView mQueueSizeTextView;
     private TextView mWorkerCountTextView;
@@ -37,24 +45,80 @@ public class RunActivity extends AppCompatActivity {
         mCompleteCountTextView = (TextView) findViewById(R.id.complete_count);
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mInitPoolSize = Integer.getInteger(
-                sharedPreferences.getString(SettingsActivity.KEY_INITIAL_POOL_SIZE, null), 5);
-        mMaxPoolSize = Integer.getInteger(
-                sharedPreferences.getString(SettingsActivity.KEY_MAX_POOL_SIZE, null), 5);
-        mKeepAlive = Integer.getInteger(
-                sharedPreferences.getString(SettingsActivity.KEY_KEEPALIVE, null), 1);
+        mInitPoolSize = Integer.parseInt(
+                sharedPreferences.getString(SettingsActivity.KEY_INITIAL_POOL_SIZE, "5"));
+        mMaxPoolSize = Integer.parseInt(
+                sharedPreferences.getString(SettingsActivity.KEY_MAX_POOL_SIZE, "5"));
+        mKeepAlive = Integer.parseInt(
+                sharedPreferences.getString(SettingsActivity.KEY_KEEPALIVE, "1"));
         mKeepAliveTimeUnit = TimeUnit.valueOf(
                 sharedPreferences.getString(SettingsActivity.KEY_KEEPALIVE_TIMEUNIT, "Seconds")
                     .toUpperCase());
-        mTotalTasks = Integer.getInteger(
-                sharedPreferences.getString(SettingsActivity.KEY_NUM_TASKS, null), 200);
+        mTotalTasks = Integer.parseInt(
+                sharedPreferences.getString(SettingsActivity.KEY_NUM_TASKS, "200"));
 
+        createThreadPool();
         updateUI();
+    }
+
+    private void createThreadPool() {
+        mHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == TASK_COMPLETE) {
+                    mCompletedTasks++;
+                    updateUI();
+                } else {
+                    super.handleMessage(msg);
+                }
+            }
+        };
+
+        mThreadPoolExecutor = new ThreadPoolExecutor(
+                mInitPoolSize,
+                mMaxPoolSize,
+                mKeepAlive,
+                mKeepAliveTimeUnit,
+                mRunnableQueue);
+        for (int i = 0; i < mTotalTasks; i++) {
+            mThreadPoolExecutor.execute(new BusyRunnable(this));
+        }
     }
 
     private void updateUI() {
         mQueueSizeTextView.setText(Integer.toString(mRunnableQueue.size()));
-        mWorkerCountTextView.setText("TODO");
+        mWorkerCountTextView.setText(String.format("%d/%d/%d",
+                mThreadPoolExecutor.getActiveCount(),
+                mThreadPoolExecutor.getPoolSize(),
+                mMaxPoolSize));
         mCompleteCountTextView.setText(Integer.toString(mCompletedTasks));
+    }
+
+    public void taskEnded() {
+        Message completeMessage = mHandler.obtainMessage(TASK_COMPLETE);
+        completeMessage.sendToTarget();
+    }
+
+    private void stopAll() {
+        BusyRunnable[] runnables = new BusyRunnable[mRunnableQueue.size()];
+        mRunnableQueue.toArray(runnables);
+        synchronized (this) {
+            for (int i = 0; i < runnables.length; i++) {
+                Thread t = runnables[i].mThread;
+                if (t != null) {
+                    t.interrupt();
+                }
+            }
+        }
+    }
+
+    public void stopClicked(View v) {
+        onBackPressed();
+    }
+
+    @Override
+    public void onBackPressed() {
+        stopAll();
+        super.onBackPressed();
     }
 }
