c470a0dbb02b35fe1f6268b13a9913f1874d1a2c
Joel May <joel@jmay.us>
2017-11-01 20:57:16 -0500

    Launch preferences

--- Homework4/app/src/main/java/cpre388/jmay/homework4/MainActivity.java
+++ Homework4/app/src/main/java/cpre388/jmay/homework4/MainActivity.java
@@ -1,13 +1,24 @@
 package cpre388.jmay.homework4;
 
+import android.content.Intent;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void settingsButtonClick(View v) {
+        Intent i = new Intent(this, SettingsActivity.class);
+        startActivity(i);
     }
 }
