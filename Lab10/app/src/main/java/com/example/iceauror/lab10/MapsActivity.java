011e46a0a84d1797c7affce77c44c33bf7b04712
Joel May <joel@jmay.us>
2017-11-03 11:58:50 -0500

    Set compass and zoom controls.  Set animations and map style

--- Lab10/app/src/main/java/com/example/iceauror/lab10/MapsActivity.java
+++ Lab10/app/src/main/java/com/example/iceauror/lab10/MapsActivity.java
@@ -5,13 +5,23 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.util.Scanner;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -41,8 +51,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // TODO Add a marker in your hometown
+        MarkerOptions hometown = new MarkerOptions();
+        hometown.position(new LatLng(45.613575,-122.563568));
+        hometown.title("Joel's Hometown");
+        mMap.addMarker(hometown);
         // TODO Move the camera to the Marker Location
+        CameraPosition pos = new CameraPosition(hometown.getPosition(), 9.5f, 0, 0);
+        CameraUpdate hometownMove = CameraUpdateFactory.newCameraPosition(pos);
+        mMap.animateCamera(hometownMove);
         // TODO Change the initial states of the Map.
         // TODO Add the styling to the Map
+        String nightJson = getMapStyle(R.raw.mapstyle_night);
+        MapStyleOptions styleOptions = new MapStyleOptions(nightJson);
+        mMap.setMapStyle(styleOptions);
+    }
+
+    private String getMapStyle(int resId) {
+        // from https://stackoverflow.com/questions/309424/read-convert-an-inputstream-to-a-string
+        InputStream stream = getResources().openRawResource(R.raw.mapstyle_night);
+        Scanner s = new Scanner(stream);
+        s.useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
     }
 }
