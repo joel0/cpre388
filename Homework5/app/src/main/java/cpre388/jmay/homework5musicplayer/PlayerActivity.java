f4d8d16f8ae1c8101cea7be55befc6d607a905f2
Joel May <joel@jmay.us>
2017-11-26 17:10:15 -0600

    Started homework 5

---
+++ Homework5/app/src/main/java/cpre388/jmay/homework5musicplayer/PlayerActivity.java
@@ -0,0 +1,13 @@
+package cpre388.jmay.homework5musicplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
