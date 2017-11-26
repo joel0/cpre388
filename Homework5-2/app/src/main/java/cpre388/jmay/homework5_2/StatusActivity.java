da777942e72c1e2f75ff7438d4b74054a521f13a
Joel May <joel@jmay.us>
2017-11-26 17:14:16 -0600

    Created homework5 server

---
+++ Homework5-2/app/src/main/java/cpre388/jmay/homework5_2/StatusActivity.java
@@ -0,0 +1,13 @@
+package cpre388.jmay.homework5_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+    }
+}
