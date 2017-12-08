8c3abca00771751688b9e2f8de1cb06fe41f10fc
Joel May <joel@jmay.us>
2017-12-07 20:45:37 -0600

    Created test web server

--- FinalProject/app/src/main/java/cpre388/jmay/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/cpre388/jmay/finalproject/MainActivity.java
@@ -3,11 +3,45 @@ package cpre388.jmay.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.jboss.com.sun.net.httpserver.*;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread t = new Thread(new Network());
+        t.start();
+    }
+
+    private class Network implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                HttpServer server = HttpServer.create();
+                server.bind(new InetSocketAddress(8080), 0);
+                server.createContext("/", new MyHandler());
+                server.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class MyHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello world!";
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        }
     }
 }
